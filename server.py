78d11b23992da255aebadd92307884337d64199b
shpendm <shpendmahmuti@gmail.com>
2016-11-13 06:19:39 +0100

    Updated server.py with a lighter version which can be tested

--- server.py
+++ server.py
@@ -2,34 +2,25 @@
 
 import argparse
 import os
-
 import flask
-import markdown
 from flask import Flask
 from flask import request, current_app
 from flask.json import JSONEncoder
 import optparse
 import time
 import sys, getopt, io
-from flask import render_template
-from flask import Markup
 
 from tagger import Tagger
 
-class MiniJSONEncoder(JSONEncoder):
-    """Minify JSON output."""
-    item_separator = ','
-    key_separator = ':'
-
 app = Flask(__name__)
 
-app.json_encoder = MiniJSONEncoder
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False  # see http://flask.pocoo.org/docs/0.10/config/
 
 tagger = Tagger()
-tagger.add_name("shpendmahmuti", "-111", "123")  # stub when testing
-tagger.load_global("dics/worm_global.tsv") #tagger_global
-tagger.load_names("dics/worm_entities.tsv", "dics/worm_names.tsv")
+tagger.add_name("shpendm", "-111", "123")  # stub when testing
+tagger.add_name("p511", "-12", "1234")  # stub when testing
+tagger.load_global("dics/tagger_global.tsv") #tagger_global  #dics/worm_global
+tagger.load_names("dics/tagger_entities.tsv", "dics/tagger_names.tsv") #worm_entities, worm_names
 # TODO perhaps add groups
 
 # -----------------------------------------------------------------------------------
@@ -46,101 +37,18 @@ def matches_to_simple_json(matches):
 
 # -----------------------------------------------------------------------------------
 
-@app.route('/')
-def form():
-    return """
-        <html>
-            <body>
-
-                <h1>Tagging</h1>
-                <form action="/" method="post" enctype="multipart/form-data">
-		  <fieldset>
-		    <legend>Run the docker with parameters below</legend>
-                    <p> Tagger Global <input type="file" name="filename2" style="margin-left:80px;" />
-                    <p> Tagger Entities <input type="file" name="filename3" style="margin-left:75px;" />
-                    <p> Tagger Names <input type="file" name="filename4" style="margin-left:80px;" />
-                    <p>
-			<label>Add Name, Type and ID</label>
-			  <input type = "text" id = "name_input"  value = "" />
-			  <input type = "text" id = "type_input"  value = "" />
-			  <input type = "text" id = "id_input"  value = "" />
-		    </p>
-                    <p><button type="submit"> Submit data </button>
-		  </fieldset>
-                </form>
-		
-		 <h1>Search for tagging</h1>
-		    <form action="/tests" method="post" enctype="multipart/form-data">
-		      <fieldset>
-			<legend>Input the values</legend>
-			<p>
-			  <label>Text</label>
-			  <input type = "text" id = "id_text" value = "" />
-			</p>
-			<p>
-			  <label>Entity type</label>
-			  <input type = "text" id = "id_entity_type" value = "" />
-			</p>
-			<p>
-			  <button type="submit"> Tag values </button>
-		      </fieldset>
-		    </form>
-
-             <!--   <form action="/results" method="post" enctype="multipart/form-data">
-                    <p><b> Show the content of your chosen file</b></p>
-                    <p> Pick a file <input type="file" name="filename" />
-                    <button type="submit"> Show content </button>
-                </form>
-	     -->
-            </body>
-        </html>
-    """
-
-"""
-	@app.route('/results', methods=["POST"])
-	def results_view():
-	    f = request.files['filename']
-	    if not f:
-		return "No file"
-
-	    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-	    lines =''
-	    val = 0
-	    while True:
-		    val+=1
-		    read_data = stream.readline()
-		    lines += '<p> Line nr: %d ----- %s</p>' % (val, read_data)
-		    if bool(read_data)==False:
-			break
-
-	    return lines
-"""
-@app.route('/tests', methods=["POST"])
-def tests_view():
-    f = request.files['filename']
-    if not f:
-        return "No file"
-
-    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-    lines =''
-    val = 0
-    while True:
-            val+=1
-	    read_data = stream.readline()
-	    lines += '<p> Line nr: %d ----- %s</p>' % (val, read_data)
-	    if bool(read_data)==False:
-		break
-
-    return lines
-
-@app.route("/annotate/<entity_types>", methods=['POST','PUT','GET'])#, methods=['PUT', 'POST'])
-def annotate(entity_types, text=None): #(): #(entity_types, text=None):
-    """
-    entity_types: e.g., "9606,-3,-22"
-    """
-
-    text = "shpendmahmuti" #text = 13 chars
-    entity_types = "2, -10, -111"  #type=-1, id=123  (type: if one number is on the document, then it tags it)
+@app.route('/annotate', methods=['GET', 'POST'])
+def annotate(entity_types=None, text=None):
+    print request.args.get('entity_types') #example "-111" or "-111,2,15" etc (numbers)
+    print request.args.get('text') #example "shpendm", "shpen-dm", "SHpend-m", "-shpend-m", ".SH-pendm" etc.  ("sh.pendm", "shpendm-" shpendmm" return empty result)
+   
+    # basically anything separated by a space before or after the correct text is tagged and the position is noted
+    # example: sadasd.-SHp-endm%20eshte is tagged. the result is: {"entities":[{"end":16,"entities":[{"id":"123","type":-111}],"start":8}]}
+    #test case: http://localhost:5000/annotate?entity_types=10090,9606,1856129&text=p53%20dhe%20.p53%20dhe%20P53.1%20dhe%20P5-3%20dhe%20-p53%20and%20nucleus%20and%20human 
+    text = request.args.get('text')
+    entity_types = request.args.get('entity_types')
+    text = str(text)
+    entity_types = str(entity_types)
 
     if text is None:
         text = request.args.get('text')
@@ -155,21 +63,14 @@ def annotate(entity_types, text=None): #(): #(entity_types, text=None):
 
     try:
         json = flask.jsonify(json)
+	json.status_code = 200
     except RuntimeError as e:
         if str(e) == 'working outside of application context':
             pass
         else:
             raise
 
-    return json #test : "It is working!"
-
-# -----------------------------------------------------------------------------------
-
-# Simple text
-with app.app_context():
-    # within this block, current_app points to app.
-    annotate("2, -111","shpendm") #()#("1", "shpendm")
-    print current_app.name
+    return json
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python server.py -p ")
@@ -178,4 +79,5 @@ if __name__ == '__main__':
     if args.port == None:
         print "Missing required argument: -p/--port"
         sys.exit(1)
+    
     app.run(host='0.0.0.0', port=int(args.port), debug=False)
