3495f21b1bcdceca20f00ce34dbf9a75889d7c9e
shpendm <shpendmahmuti@gmail.com>
2016-12-08 09:47:16 +0100

    post instead of get AND code cleaning

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import argparse
 import os
 import flask
 from flask import Flask
-from flask import request, current_app
+from flask import request, current_app, json
 from flask.json import JSONEncoder
 import optparse
 import time
@@ -21,8 +21,7 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False  # see http://flask.pocoo.org/docs/0.10/config/
 
 tagger = Tagger()
-tagger.add_name("shpendm", "-111", "123")  # stub when testing
-tagger.add_name("p511", "-12", "1234")  # stub when testing
+#tagger.add_name("p511", "-12", "1234")  # stub when testing
 tagger.load_global("dics/tagger_global.tsv") #tagger_global  #dics/worm_global
 tagger.load_names("dics/tagger_entities.tsv", "dics/tagger_names.tsv") #worm_entities, worm_names
 # TODO perhaps add groups
@@ -58,9 +57,6 @@ def tsvFilesList():
 
 # test json samples
 json1 = '{"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606},{"id":"ENSMUSP00000029699","type":10090}]}'
-json2 = '{"entities":[{"end":3,"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606}],"start":0},{"end":7,"entities":[{"id":"ENSMUSP00000029699","type":10090}],"start":4},{"end":12,"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606}],"start":9},{"end":16,"entities":[{"id":"ENSMUSP00000029699","type":10090}],"start":13},{"end":20,"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606}],"start":17},{"end":26,"entities":[{"id":"ENSMUSP00000029699","type":10090}],"start":23},{"end":31,"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606}],"start":27},{"end":35,"entities":[{"id":"ENSMUSP00000029699","type":10090}],"start":32},{"end":40,"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606}],"start":37}]}'
-json3 = '{"entities":[{"end":183,"entities":[{"id":"ENSP00000358525","type":9606}],"start":180},{"end":188,"entities":[{"id":"ENSP00000431418","type":9606}],"start":184},{"end":252,"entities":[{"id":"ENSMUSP00000110115","type":10090},{"id":"ENSP00000264122","type":9606},{"id":"ENSP00000445920","type":9606}],"start":247},{"end":350,"entities":[{"id":"ENSP00000358525","type":9606}],"start":331},{"end":355,"entities":[{"id":"ENSP00000358525","type":9606}],"start":352},{"end":412,"entities":[{"id":"ENSP00000431418","type":9606}],"start":384},{"end":418,"entities":[{"id":"ENSP00000431418","type":9606}],"start":414},{"end":549,"entities":[{"id":"ENSP00000358525","type":9606}],"start":546},{"end":554,"entities":[{"id":"ENSP00000431418","type":9606}],"start":550},{"end":780,"entities":[{"id":"ENSP00000358525","type":9606}],"start":777},{"end":796,"entities":[{"id":"ENSP00000431418","type":9606}],"start":792},{"end":821,"entities":[{"id":"ENSP00000358525","type":9606}],"start":818},{"end":839,"entities":[{"id":"ENSP00000431418","type":9606}],"start":835},{"end":878,"entities":[{"id":"ENSMUSP00000110115","type":10090},{"id":"ENSP00000264122","type":9606},{"id":"ENSP00000445920","type":9606}],"start":873},{"end":999,"entities":[{"id":"ENSMUSP00000110115","type":10090},{"id":"ENSP00000264122","type":9606},{"id":"ENSP00000445920","type":9606}],"start":994},{"end":1013,"entities":[{"id":"ENSP00000431418","type":9606}],"start":1009},{"end":1106,"entities":[{"id":"ENSMUSP00000101471","type":10090},{"id":"ENSP00000363763","type":9606},{"id":"ENSMUSP00000023462","type":10090},{"id":"ENSP00000215832","type":9606}],"start":1103},{"end":1149,"entities":[{"id":"ENSMUSP00000110115","type":10090},{"id":"ENSP00000264122","type":9606},{"id":"ENSP00000445920","type":9606}],"start":1144},{"end":1228,"entities":[{"id":"ENSMUSP00000110115","type":10090},{"id":"ENSP00000264122","type":9606},{"id":"ENSP00000445920","type":9606}],"start":1223},{"end":1239,"entities":[{"id":"ENSP00000358525","type":9606}],"start":1236},{"end":1244,"entities":[{"id":"ENSP00000431418","type":9606}],"start":1240}]}\n'
-
 # -----------------------------------------------------------------------------------
 
 # convert StringID to UniprotID
@@ -94,7 +90,6 @@ def stringIDtoUniprotID(values):
     # dictionary of common organisms
     #dict = {'10090': 'mouse', '7955': 'else0', '9060': 'human', '3702':'else1', '4896':'else2', '4932':'else3', '511145':'else4', '6239':'else5', '7227':'else6'}
 
-
     # find conversion of StringID to UniprotID
     def StrToUni(strId, entity):
         lookup = strId
@@ -126,12 +121,41 @@ def stringIDtoUniprotID(values):
 
 # -----------------------------------------------------------------------------------
 
-@app.route('/annotate', methods=['GET', 'POST'])
-def annotate(entity_types=None, text=None):
-    print request.args.get('text') #example "shpendm", "shpen-dm", "SHpend-m", "-shpend-m", ".SH-pendm" etc.  ("sh.pendm", "shpendm-" shpendmm" return empty result)
+@app.route('/annotate/post', methods=['POST'])
+def annotatePost():
+
+    text = json.dumps(request.json.get('text'))
+
+    document_id_stub = 1
+    entity_types="-22,9606" #-3,-22,9606
+    entity_types = set([int(x) for x in entity_types.split(",")])
+
+    matches = tagger.get_matches(text, document_id_stub, entity_types, auto_detect=True, protect_tags=False)
+    jsonOut = matches_to_simple_json(matches)
+    jsonOut = json.dumps(jsonOut)
+    jsonOut = jsonOut.replace("'",'"')
+    jsonOut = stringIDtoUniprotID(jsonOut)
+    jsonOut = json.loads(jsonOut)
+
+    try:
+        jsonOut = flask.jsonify(jsonOut)
+	jsonOut.status_code = 200
+    except RuntimeError as e:
+        if str(e) == 'working outside of application context':
+            pass
+        else:
+            raise
+
+    return jsonOut
+
+
+'''
+
+@app.route('/annotate', methods=['GET'])
+def annotateGet(text=None):
+    print(request.args.get('text'))
 
     # basically anything separated by a space before or after the correct text is tagged and the position is noted
-    # example: sadasd.-SHp-endm%20eshte is tagged. the result is: {"entities":[{"end":16,"entities":[{"id":"123","type":-111}],"start":8}]}
     #test case: http://localhost:5000/annotate?text=p53%20human%20mouse%20human%20tp53
     text = request.args.get('text')
     text = str(text)
@@ -146,15 +170,13 @@ def annotate(entity_types=None, text=None):
     entity_types = set([int(x) for x in entity_types.split(",")])
 
     matches = tagger.get_matches(text, document_id_stub, entity_types, auto_detect=True, protect_tags=False)
-    print matches
     jsonOut = matches_to_simple_json(matches)
     #jsonOut ='{"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606},{"id":"ENSMUSP00000029699","type":10090}]}'
-    #print type(jsonOut)
     jsonOut = json.dumps(jsonOut)
     jsonOut = jsonOut.replace("'",'"')
     jsonOut = stringIDtoUniprotID(jsonOut)
     jsonOut = json.loads(jsonOut)
-    #print js
+
     try:
         jsonOut = flask.jsonify(jsonOut)
 	jsonOut.status_code = 200
@@ -163,16 +185,18 @@ def annotate(entity_types=None, text=None):
             pass
         else:
             raise
-    #print stringIDtoUniprotID(json1)
-    print str(jsonOut)
-    return jsonOut #JSONEncoder().encode(jsonOut) #str(jsonOut)  #json.loads(stringIDtoUniprotID(json.dumps(json1))) #stringIDtoUniprotID(json1) #json.loads(stringIDtoUniprotID(jsonOut))
+
+    return jsonOut
+
+'''
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python server.py -p ")
     parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen.')
     (args, _) = parser.parse_args()
     if args.port == None:
-        print "Missing required argument: -p/--port"
+        print("Missing required argument: -p/--port")
         sys.exit(1)
 
     app.run(host='0.0.0.0', port=int(args.port), debug=False)
+
