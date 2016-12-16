24b9717256ba49eb91e192ea04e1e9e6bbded0ec
shpendm <shpendmahmuti@gmail.com>
2016-12-16 22:17:29 +0100

    fixed TODOs except for pandas part

--- server.py
+++ server.py
@@ -20,52 +20,36 @@ app = Flask(__name__)
 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False  # see http://flask.pocoo.org/docs/0.10/config/
 
+REVIEWED_UNIPROT_2_STRING_DATE = '04_2015'
+
 tagger = Tagger()
 
-# TODO here and below, delete all old-stub-dummy comments
-# TODO use pep8 to follow the python syntax recommendations --> no warning or error should be shown
-# TODO remove the mixed use of tabs and spaces --> use spaces only
-# TODO variables & function names are_like_this, NOT areLikeThis
 # TODO replace pandas by simple tsv python library reader
-# TODO are you sure from the Dockerfile definition that we are using indeed Python 3 ??
 
-tagger.load_global("dics/tagger_global.tsv") #tagger_global  #dics/worm_global
-tagger.load_names("dics/tagger_entities.tsv", "dics/tagger_names.tsv") #worm_entities, worm_names
+tagger.load_global("dics/tagger_global.tsv")
+tagger.load_names("dics/tagger_entities.tsv", "dics/tagger_names.tsv")
+
 
 # -----------------------------------------------------------------------------------
 
 def matches_to_simple_json(matches):
-    aList = list()
+    a_list = list()
     for a in matches:
         for e in a[2]:
-            if(e[0]==-3 or e[0]==-22):
-                resp= list()
-                odict =  OrderedDict([('id2', e[1]), ('type2', e[0])])
-		resp.insert(0,odict)
+            if (e[0] == -3 or e[0] == -22):
+                resp = list()
+                odict = OrderedDict([('id2', e[1]), ('type2', e[0])])
+                resp.insert(0, odict)
             else:
-                resp= list()
-                odict =  OrderedDict([('id', e[1]), ('type', e[0])])
-                odict2 = OrderedDict([('id2', 'toreplace'+e[1]), ('type2', 'uniprot:' + str(e[0]))])
-                resp.insert(0,odict)
-                resp.insert(1,odict2)
-            createMatching = OrderedDict([('end', a[1]),('normalizations', resp), ('start', a[0])])
-        aList.append(createMatching)
-
-    out = {'entities': aList}
+                resp = list()
+                odict = OrderedDict([('id', e[1]), ('type', e[0])])
+                odict2 = OrderedDict([('id2', 'toreplace' + e[1]), ('type2', 'uniprot:' + str(e[0]))])
+                resp.insert(0, odict)
+                resp.insert(1, odict2)
+        create_matching = OrderedDict([('end', a[1]), ('normalizations', resp), ('start', a[0])])
+        a_list.append(create_matching)
+    out = {'entities': a_list}
     return out
-
-# -----------------------------------------------------------------------------------
-
-# Read names from uniprot files
-def tsvFilesList():
-    filesList = []
-    for filename in os.listdir('/app/tagger'): #/uniprot
-        if filename.endswith(".tsv"):
-            filesList.append(filename)
-            continue
-        else:
-            continue
-    return filesList
 
 
 # -----------------------------------------------------------------------------------
@@ -75,70 +59,66 @@ def string_id_to_uniprot_id(values):
     # find all IDs and types corresponding to them
     def find_values(id, json_repr):
         results = []
+
         def _decode_dict(a_dict):
-            try: results.append(a_dict[id])
-            except KeyError: pass
+            try:
+                results.append(a_dict[id])
+            except KeyError:
+                pass
             return a_dict
 
         json.loads(json_repr, object_hook=_decode_dict)
         return results
 
-
     # format ids and types in a dictionary format
-    def jsonIDsAndTypes(jsonText):
-        #find all occurrences of ids and types
-        ids1 = find_values('id', jsonText)
-        types1 = find_values('type', jsonText)
+    def json_ids_and_types(json_text):
+        # find all occurrences of ids and types
+        ids1 = find_values('id', json_text)
+        types1 = find_values('type', json_text)
 
         types = [item for item in types1 if (item != -3 or item != -22)]
-	#types = types1
         ids = [x for x in ids1 if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
 
         # create a ordered dictionary with ids and types
-        new_dict = OrderedDict(zip(ids, types)) #new_dict = OrderedDict(zip(ids, types))
-        return(new_dict)
-
+        new_dict = OrderedDict(zip(ids, types))  # new_dict = OrderedDict(zip(ids, types))
+        return (new_dict)
 
     # dictionary of common organisms
-    # TODO the date "04_2015" should be a constant defined at the beginning: REVIEWED_UNIPROT_2_STRING = '04_2015'
-    common_organisms = {'10090': '10090_reviewed_uniprot_2_string.04_2015.tsv',
-                        '9606': '9606_reviewed_uniprot_2_string.04_2015.tsv',
-                        '3702': '3702_reviewed_uniprot_2_string.04_2015.tsv',
-                        '4896': '4896_reviewed_uniprot_2_string.04_2015.tsv',
-                        '4932': '4932_reviewed_uniprot_2_string.04_2015.tsv',
-                        '511145': '511145_reviewed_uniprot_2_string.04_2015.tsv',
-                        '6239': '6239_reviewed_uniprot_2_string.04_2015.tsv',
-                        '7227': '7227_reviewed_uniprot_2_string.04_2015.tsv',
-                        '7955': '7955_reviewed_uniprot_2_string.04_2015.tsv',
-                        'full': 'full_uniprot_2_string.04_2015.tsv',
+    common_organisms = {'10090': '10090_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '9606': '9606_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '3702': '3702_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '4896': '4896_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '4932': '4932_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '511145': '511145_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '6239': '6239_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '7227': '7227_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        '7955': '7955_reviewed_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
+                        'full': 'full_uniprot_2_string.' + REVIEWED_UNIPROT_2_STRING_DATE + '.tsv',
                         }
 
-
     # find conversion of StringID to UniprotID
-    def StrToUni(strId, entity):
-        lookup = strId
+    def str_to_uni(str_id, entity):
         indx = None
         with open(entity) as tsvFile:
             for num, line in enumerate(tsvFile, -1):
-                if lookup in line:
+                if str_id in line:
                     indx = num
         df = pd.read_csv(entity, sep='\t')
         df = df.rename(columns={'#species   uniprot_ac|uniprot_id   string_id   identity   bit_score': 'col'})
-        return(str(df.iloc[[indx]]).split()[1 + 1])
+        return (str(df.iloc[[indx]]).split()[1 + 1])
 
     # loop through all occurrences of ids for each type
-    for i in range(0,(len(jsonIDsAndTypes(values).keys()))):
+    for i in range(0, (len(json_ids_and_types(values).keys()))):
         # get the complete filename that contains the type_name
-        #matching = [s for s in tsvFilesList() if str(list(jsonIDsAndTypes(values).values())[i]) in s]
-	matching = common_organisms[str(list(jsonIDsAndTypes(values).values())[i])]
+        matching = common_organisms[str(list(json_ids_and_types(values).values())[i])]
         # find replacements of stringID with uniprotId in tsv and change values of json object
-        values = values.replace('toreplace'+str(list(jsonIDsAndTypes(values).keys())[i]),
-                             StrToUni(str(list(jsonIDsAndTypes(values).keys())[i]), str(matching)))
-#StrToUni(str(list(jsonIDsAndTypes(values).keys())[i]), str(matching).strip('\'[]\'')))
+        values = values.replace('toreplace' + str(list(json_ids_and_types(values).keys())[i]),
+                                str_to_uni(str(list(json_ids_and_types(values).keys())[i]), str(matching)))
 
     values = re.sub("id2", "id", values)
     values = re.sub("type2", "type", values)
-    return(values)
+    return (values)
+
 
 # -----------------------------------------------------------------------------------
 
@@ -146,90 +126,47 @@ def string_id_to_uniprot_id(values):
 def root():
     return 'Welcome'
 
+
 # -----------------------------------------------------------------------------------
 
 @app.route('/annotate/post', methods=['POST'])
 def annotate_post():
-
     entity_types = request.json.get('ids')
     if entity_types is None:
-        entity_types="-22,9606" #default
+        entity_types = "-22,9606"  # default
 
     text = json.dumps(request.json.get('text'))
 
     auto_detect = request.json.get('autodetect')
-    if auto_detect is None or auto_detect !="False":
-        auto_detect=True
+    if auto_detect is None or auto_detect != "False":
+        auto_detect = True
     else:
-	auto_detect = False
-
+        auto_detect = False
 
     document_id_stub = 1
     entity_types = set([int(x) for x in entity_types.split(",")])
 
     matches = tagger.get_matches(text, document_id_stub, entity_types, auto_detect, protect_tags=False)
-    jsonOut = matches_to_simple_json(matches)
-    jsonOut = json.dumps(jsonOut)
-    jsonOut = jsonOut.replace("'",'"')
-    jsonOut = string_id_to_uniprot_id(jsonOut)
-    jsonOut = json.loads(jsonOut)
+    json_out = matches_to_simple_json(matches)
+    json_out = json.dumps(json_out)
+    json_out = json_out.replace("'", '"')
+    json_out = string_id_to_uniprot_id(json_out)
+    json_out = json.loads(json_out)
 
     try:
-        jsonOut = flask.jsonify(jsonOut)
-	jsonOut.status_code = 200
+        json_out = flask.jsonify(json_out)
+        json_out.status_code = 200
     except RuntimeError as e:
         if str(e) == 'working outside of application context':
             pass
         else:
             raise
 
-    return jsonOut
+    return json_out
 
-
-# TODO what is this? Delete
-'''
-
-@app.route('/annotate', methods=['GET'])
-def annotateGet(text=None):
-    print(request.args.get('text'))
-
-    # basically anything separated by a space before or after the correct text is tagged and the position is noted
-    #test case: http://localhost:5000/annotate?text=p53%20human%20mouse%20human%20tp53
-    text = request.args.get('text')
-    text = str(text)
-
-    if text is None:
-        text = request.args.get('text')
-    if text is None:
-        text = request.form['text']
-
-    document_id_stub = 1
-    entity_types="-22,9606" #-3,-22,9606
-    entity_types = set([int(x) for x in entity_types.split(",")])
-
-    matches = tagger.get_matches(text, document_id_stub, entity_types, auto_detect=True, protect_tags=False)
-    jsonOut = matches_to_simple_json(matches)
-    #jsonOut ='{"entities":[{"id":"ENSMUSP00000104298","type":10090},{"id":"ENSP00000269305","type":9606},{"id":"ENSMUSP00000029699","type":10090}]}'
-    jsonOut = json.dumps(jsonOut)
-    jsonOut = jsonOut.replace("'",'"')
-    jsonOut = string_id_to_uniprot_id(jsonOut)
-    jsonOut = json.loads(jsonOut)
-
-    try:
-        jsonOut = flask.jsonify(jsonOut)
-	jsonOut.status_code = 200
-    except RuntimeError as e:
-        if str(e) == 'working outside of application context':
-            pass
-        else:
-            raise
-
-    return jsonOut
-
-'''
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser(usage="python server.py -p ")
+    parser = optparse.OptionParser(usage="python3.4 server.py -p ")
     parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen.')
     (args, _) = parser.parse_args()
     if args.port == None:
